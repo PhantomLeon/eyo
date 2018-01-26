0253d6f9d63de31be7915b22fc0a9622b4a0a841
PhantomLeon <leonrick512@gmail.com>
2018-01-26 14:09:10 +0100

    lele

--- src/H6/H60PraktijkOpdracht.java
+++ src/H6/H60PraktijkOpdracht.java
@@ -5,4 +5,26 @@ import java.applet.*;
 
 public class H60PraktijkOpdracht extends Applet {
 
+    double a , b , c;
+    double uitkomst;
+
+
+    public void init() {
+        a = 7.9;
+        b = 8.3;
+        c = 10.4;
+        uitkomst = (a + b + c) / 3;
+
+
+
+    }
+
+    //Het lukte niet helemaal perfect zoals het moest
+
+
+    public void paint(Graphics g) {
+        g.drawString("Het gemiddlede is: " + uitkomst, 20, 20);
+        g.setColor(Color.white);
+        g.fillRect(145, 10, 600, 50);
+    }
 }
